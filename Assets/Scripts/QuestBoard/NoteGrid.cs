167ff969e3130449b9b6097db34d150250a3231e
Peter Witt <peter.witt.joergensen@gmail.com>
2017-11-25 15:17:55 +0100

    QuestBoard 1.st commig

---
+++ Assets/Scripts/QuestBoard/NoteGrid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace QuestBoard
+{
+    /// <summary>
+    /// Purpose:
+    /// Creator:
+    /// </summary>
+    public class NoteGrid : MonoBehaviour
+    {
+        [SerializeField] private GameObject _note;
+
+
+    }
+}
