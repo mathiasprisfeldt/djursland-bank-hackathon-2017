9ac002da52272f397e1b839f657417cc4938ee89
Mikkel Bruun <mikkel.bn96@gmail.com>
2017-11-25 22:34:52 +0100

    Added roomchanger

---
+++ Assets/Scripts/RoomChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChanger : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _spites = new List<Sprite>();
+
+    [SerializeField] private SpriteRenderer _target;
+
+    private int _index;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _target.sprite = _spites[_index];
+	}
+
+    public void NextSprite()
+    {
+        _index++;
+        if (_index >= _spites.Count)
+            _index = 0;
+        _target.sprite = _spites[_index];
+    }
+}
